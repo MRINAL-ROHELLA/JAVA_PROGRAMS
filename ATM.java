491740f590d225897427bebd0236739817c05c33
MRINAL-ROHELLA <mrinalrohella@gmail.com>
2023-10-10 17:38:20 +0530

    java

--- ATM.java
+++ ATM.java
@@ -6,15 +6,40 @@ public class ATM {
         System.out.println("Enter 6 digit Account num: ");
         int acc = scann.nextInt();
         int LEN = String.valueOf(acc).length();
+        int amnt=0;
+        int dep=0;
+        int with=0;
+        
         if(LEN==6){
-
-            System.out.println("Choose Option\n 1. FOR BALANCE\n 2. FOR DEPOSIT\n3. FOR WITHDRAW");
+            while(acc!=0){
+            System.out.println("Choose Option\n1. FOR BALANCE\n2. FOR DEPOSIT\n3. FOR WITHDRAW\n4. FOR EXIT");
             int OP = scann.nextInt();
             switch(OP){
                 case 1:
-                System.out.println("YOUR BALANCE IS: ");
+                System.out.println("YOUR BALANCE IS: "+(amnt));
+                break;
+                case 2:
+                System.out.println("ENTER AMOUNT TO DEPOSIT: ");
+                dep=scann.nextInt();
+                amnt= amnt+dep;
+                break;
+                case 3:
+                System.out.println("ENTER AMOUNT TO WITHDRAW: ");
+                with=scann.nextInt();
+                amnt=amnt-with;
+                break;
+                case 4:
+                System.out.println("THANKYOU:)");
+                acc=0;
+                break;
+                default:
+                System.out.println("WRONG CHOICE!!!");
+                
             }
 
+        }}
+        else{
+            System.out.println("INVALID ACCOUNT NUMBER!!!");
         }
         scann.close();
 
